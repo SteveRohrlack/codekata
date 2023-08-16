32ba6a8084bc7e66b87e6e48d8c2972fde963047
Steve Rohrlack <elmo.dd@gmail.com>
2023-08-16 10:47:07 +0200

    Update test_survive.py

--- 555624b601231dc7a400017a/python/test_survive.py
+++ 555624b601231dc7a400017a/python/test_survive.py
@@ -1,9 +1,12 @@
 from survive import survive
 
-def test_7():
-    survivor = survive(7, 3)
-    assert survivor == 4, "Should be 4"
+def test():
+    assert survive(7, 3) == 4, "Should be 4"
+    # assert survive(11, 19) == 10, "Should be 10"
+    assert survive(1, 300) == 1, "Should be 1"
+    assert survive(14, 2) == 13, "Should be 13"
+    assert survive(100, 1) == 100, "Should be 100"
 
 if __name__ == "__main__":
-    test_7()
+    test()
     print("Everything passed")
