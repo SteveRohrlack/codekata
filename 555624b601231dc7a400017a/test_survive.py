9a7594979294d522f69d79535283b395313ff6e3
Steve Rohrlack <elmo.dd@gmail.com>
2023-08-16 10:23:28 +0200

    Delete test_survive.py

--- 555624b601231dc7a400017a/test_survive.py
+++
@@ -1,9 +0,0 @@
-from survive import survive
-
-def test_7():
-    survivor = survive(7, 3)
-    assert survivor == 4, "Should be 4"
-
-if __name__ == "__main__":
-    test_7()
-    print("Everything passed")
